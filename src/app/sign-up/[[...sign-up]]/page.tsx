f37a901249e295c5b8bf1878fc68809bbbee69b0
gincode18 <vishalkamboj9211@gmail.com>
2023-10-05 00:00:49 +0530

    test

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <SignIn />
+      <h1>uidefgugie</h1>
     </div>
   );
 }
