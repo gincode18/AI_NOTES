8ffa140edf62b8b7d22749256395ba0b582f7ad0
gincode18 <vishalkamboj9211@gmail.com>
2023-10-04 21:35:01 +0530

    some frontend :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+import TypewriterTitle from "@/components/ui/TypewriterTitle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
-  return <Button className="">kdgus</Button>;
+  return (
+    <div className=" min-h-screen  grainy  bg-gradient-to-r from-primary via-accent to-secondary">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7xl text-center">
+          AI <span className="text-primary font-bold hover:text-secondary transition-colors">note taking</span>{" "}
+          assistant.
+        </h1>
+        <div className="mt-4"></div>
+        <h2 className="font-semibold text-3xl text-center text-slate-700">
+          <TypewriterTitle />
+        </h2>
+        <div className="mt-8"></div>
+
+        <div className="flex justify-center">
+          <Link href="/dashboard">
+            <Button className=" hover:bg-secondary">
+              Get Started
+              <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
