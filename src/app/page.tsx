ab769228c238f71e881a7b6ee5e382d667dbd73a
gincode18 <vishalkamboj9211@gmail.com>
2023-10-04 20:25:10 +0530

    done with daisyui shadcn tailwind setup :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-<h1>hola</h1>
-  )
+  return <Button className="">kdgus</Button>;
 }
