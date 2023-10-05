8436021e6f26331650b0f7b75b86c7dd9387228c
gincode18 <vishalkamboj9211@gmail.com>
2023-10-06 01:53:25 +0530

    Finally Fixed That Clerk Bug Its 3 AM ;):)

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,10 @@
-import { SignUp} from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <SignUp />
+      <SignIn />
+      <h1>uidefgugie</h1>
     </div>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,9 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp} from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <SignIn />
-      <h1>uidefgugie</h1>
+      <SignUp />
     </div>
   );
 }
