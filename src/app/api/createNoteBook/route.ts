cd76f1df1395274e75ec66542a976b25a9a031b7
gincode18 <vishalkamboj9211@gmail.com>
2024-07-22 10:01:36 +0530

    failed to create NB fix :)

--- src/app/api/createNoteBook/route.ts
+++ src/app/api/createNoteBook/route.ts
@@ -3,37 +3,37 @@
 import { db } from "@/lib/db";
 import { $notes } from "@/lib/db/schema";
 import { generateImage, generateImagePrompt } from "@/lib/openai";
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse,NextRequest } from "next/server";
 
 export const runtime = "edge";
 
-export async function POST(req: Request) {
-  const { userId } = auth();
+export async function POST(req: NextRequest) {
+  const { userId } = getAuth(req);
   if (!userId) {
     return new NextResponse("unauthorised", { status: 401 });
   }
   const body = await req.json();
   const { name } = body;
-  const image_description = await generateImagePrompt(name);
-  if (!image_description) {
-    return new NextResponse("failed to generate image description", {
-      status: 500,
-    });
-  }
-  const image_url = await generateImage(image_description);
-  if (!image_url) {
-    return new NextResponse("failed to generate image ", {
-      status: 500,
-    });
-  }
+  // const image_description = await generateImagePrompt(name);
+  // if (!image_description) {
+  //   return new NextResponse("failed to generate image description", {
+  //     status: 500,
+  //   });
+  // }
+  // const image_url = await generateImage(image_description);
+  // if (!image_url) {
+  //   return new NextResponse("failed to generate image ", {
+  //     status: 500,
+  //   });
+  // }
 
   const note_ids = await db
     .insert($notes)
     .values({
       name,
       userId,
-      imageUrl: image_url,
+      imageUrl: "https://res.cloudinary.com/dbj5s9roc/image/upload/v1721622532/mefch35oacltvszsjlkq.jpg",
     })
     .returning({
       insertedId: $notes.id,
